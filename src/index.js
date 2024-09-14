b601c23de938497fb4e213c36433b10c1e51c4df
Adam Lee <adam@hkc.ie>
2024-09-14 19:44:00 +0100

    refactor: removed test changes and fix added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export { default as SortableListProvider } from './Config';
 export { default as SortableList } from './SortableList';
 export { default as Tile } from './Tile';
-export { default as TestView } from './TestView';
